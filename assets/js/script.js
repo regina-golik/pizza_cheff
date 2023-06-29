0f6e37d395e00e29f624158c4eb8a8a97fb54eb7
Regina <rgabi0605@yandex.ru>
2023-06-29 15:45:09 +0300

    адаптив header

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    const menuButton = $('.menu__burger');
+    const menuList = $('.menu__list');
+
+    menuButton.on('click', function () {
+        menuList.toggleClass('menu__list-opened')
+    })
+}
